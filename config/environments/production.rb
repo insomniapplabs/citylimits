f36f2ab542f25b6fba7b27652b9633829666621e
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-07-08 23:50:12 -0500

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "104.131.1.137" }
+  config.action_mailer.default_url_options = { host: "www.thecitylimitssalon.com" }
   config.action_mailer.deliver_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
