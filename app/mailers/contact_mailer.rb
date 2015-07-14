8ee978cb8865df3260072f2dd3d661d99c50842b
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-07-13 21:31:26 -0500

    change mail

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,12 +2,12 @@ class ContactMailer < ActionMailer::Base
 
   def contact_email(contact)
     @contact = contact
-    mail(to: "aaronjohnsonis@gmail.com", subject: "New Contact Message", from: @contact.email)
+    mail(to: "thecitylimitssalon@gmail.com", subject: "New Contact Message", from: @contact.email)
   end
 
   def contact_confirmation(contact)
       @contact = contact
-      mail(to: @contact.email, subject: "City Limits Salon", from: "aaronjohnsonis@gmail.com")
+      mail(to: @contact.email, subject: "City Limits Salon", from: "thecitylimitssalon@gmail.com")
   end
 
 end
